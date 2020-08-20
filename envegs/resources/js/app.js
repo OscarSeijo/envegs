9e9992e045155a150161ce051648da422949d629
OscarSeijo <oscarseijo@outlook.com>
2020-08-19 23:05:55 -0400

    Preset Vue
    Se prepara el environment con vue

--- envegs/resources/js/app.js
+++ envegs/resources/js/app.js
@@ -1,1 +1,24 @@
-require('./bootstrap');
+import Vue from 'vue';
+
+// Importamos la vista base
+import App from './App.vue';
+
+
+
+// importamos librerias
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
+
+
+// Desactivamos el modo producción
+Vue.config.productionTip = false
+
+// Aqui cargamos el vue al id App
+new Vue({
+	el: '#app',
+	render: h => h(App)
+});
+
+
+
+
